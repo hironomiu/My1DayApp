3a04e0da16dcf7128eec8477d2fba7440a29742a
hironomiu <hironomiu@example.com>
2015-04-15 10:31:16 +0900

    Initial Commit

---
+++ My1DayApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My1DayApp
+//
+//  Created by 三浦裕典 on 2015/04/15.
+//  Copyright (c) 2015年 三浦裕典. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
